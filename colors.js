d0145d4f230bdc99150a99f54262aeadc1a89df4
Artur012097 <62646972+Artur012097@users.noreply.github.com>
2021-02-18 15:16:09 +0400

    Add game
    I've uploaded the game to github

---
+++ colors.js
@@ -0,0 +1,7 @@
+let gray = '#ccc'
+let white = '#fff'
+let black = '#000'
+let boxColors = [
+    '#3252d1', '#1fff9a', '#ff4c4c', '#7a4444', '#ff03af', '#ba7ba6',
+    '#14ffff', '#57ff14', '#60cc35', '#d9ff00', '#566117', 
+]
---
+++ index.js
@@ -0,0 +1,123 @@
+// let elements from DOM
+let $game = document.querySelector('#game')
+let $start = document.querySelector('#start')
+let $timeHeader = document.querySelector('#time-header')
+let $resultHeader = document.querySelector('#result-header')
+let $result = document.querySelector('#result')
+let $gameTime = document.querySelector('#game-time')
+let $time = document.querySelector('#time')
+let score = 0
+let isGameStarted = false
+
+// show element global function
+function show($el) {
+    $el.classList.remove('hide')
+}
+
+// hide element global function
+function hide($el) {
+    $el.classList.add('hide')
+}
+
+$start.addEventListener('click', startGame)
+document.addEventListener('click', boxClick)
+$gameTime.addEventListener('input', setTime)
+
+function startGame() {
+    isGameStarted = true
+    score = 0
+    hide(start)
+    $game.style.backgroundColor = white
+    $gameTime.setAttribute('disabled', 'true')
+    setTime()
+
+    let interval = setInterval(function() {
+        // let variable vor set game time
+        let time = parseFloat($time.textContent)
+    
+        if (time <= 0) {
+            clearInterval(interval)
+            stopGame()
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1)
+        }
+    },100)
+    
+    renderBox()
+}
+
+function stopGame () {
+    isGameStarted = false
+    $game.innerHTML = ''
+    setTimeout(() => {
+        show($start)
+    }, 1000)
+    $game.style.backgroundColor = gray
+    hide($timeHeader)
+    show($resultHeader)
+    $gameTime.removeAttribute('disabled')
+    setResult()
+}
+
+function setResult() {
+    $result.textContent = score
+}
+
+function setTime() {
+    let setGameTime = $gameTime.value
+
+    $time.textContent = setGameTime + '.0'
+
+    show($timeHeader)
+    hide($resultHeader)
+}
+
+function boxClick(e) {
+    // box renders when player clicks on it
+    if (e.target.dataset.box) {
+        // score rises when player clicks on rendered box
+        score++
+        renderBox()
+    }
+}
+
+function renderBox() {
+    // clear html after render next box
+    $game.innerHTML = ''
+    // xox create after start button click
+    let box = document.createElement('div')
+    // box random size render
+    let boxSize = randomBox(30, 100)
+    // let game box styles
+    let gameBox = $game.getBoundingClientRect()
+    // random positions render for box
+    let maxTop = gameBox.height - boxSize
+    let maxLeft = gameBox.width - boxSize
+    // set box random width
+    box.style.width = boxSize + 'px'
+    // set box random height
+    box.style.height = boxSize + 'px'
+    // set box random background color from function
+    box.style.backgroundColor = letBoxBg()
+    box.style.position = 'absolute'
+    // set box random position top
+    box.style.top = randomBox(0, maxTop) + 'px'
+    // set box random position left
+    box.style.left = randomBox(0, maxLeft) + 'px'
+    box.style.cursor = 'pointer'
+    // set data atribut to box, it needs for next ranbom box render after click
+    box.setAttribute('data-box', true)
+    // set created div on game box
+    $game.insertAdjacentElement('afterbegin', box)
+}
+// random background color for rendered boxes
+function letBoxBg() {
+    return boxColors[randomBox(0, boxColors.length)]
+}
+// randomiser, random numbers from min to max
+function randomBox(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+
+
