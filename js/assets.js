f4c12414f20396e1dc96006bf58a2a84fc722cfa
Artur012097 <62646972+Artur012097@users.noreply.github.com>
2021-03-28 11:57:42 +0300

    Delete js directory

--- js/assets.js
+++
@@ -1,8 +0,0 @@
-const white = '#fff'
-const black = '#000'
-const gray = '#ccc'
-const red = '#d95959'
-let boxBg = [
-    'url(./img/zombies/zombie_1.png)', 'url(./img/zombies/zombie_2.png)', 'url(./img/zombies/zombie_3.png)',
-    'url(./img/zombies/zombie_4.png)', 'url(./img/zombies/zombie_5.png)', 'url(./img/zombies/zombie_6.png)'
-]
--- js/audio.js
+++
@@ -1,6 +0,0 @@
-let gameMusic = [
-    '../audio/game-start-mc.mp3', '../audio/game-start-wot.mp3'
-]
-let gameEndMusic = new Audio('../audio/game-end.mp3')
-let volumeChangeSound = new Audio('../audio/volume-change.mp3')
-let zombieTrackSound = new Audio('../audio/zombie-track-sound.mp3')
