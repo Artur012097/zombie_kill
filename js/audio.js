7d9c17eecd4e8be9fa914a1d498a3694d1869c97
Artur012097 <62646972+Artur012097@users.noreply.github.com>
2021-03-28 11:59:01 +0300

    Add files via upload

---
+++ js/audio.js
@@ -0,0 +1,3 @@
+let gameEndMusic = new Audio('../audio/game-end.mp3')
+let volumeChangeSound = new Audio('../audio/volume-change.mp3')
+let zombieTrackSound = new Audio('../audio/zombie-track-sound.mp3')
